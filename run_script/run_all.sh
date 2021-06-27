f65e9929ed05486d925b05ff88bdf0f894253cf1
Md. Alvee Noor <mnoor@unb.ca>
2021-06-27 03:20:25 -0300

    Configurable java, iteration, size, warmup, timed
    Signed-off-by: Md. Alvee Noor <mnoor@unb.ca>

---
+++ run_script/run_all.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+if [[ $1 != "" ]] && [[ $2 != "" ]] && [[ $3 != "" ]] && [[ $4 != "" ]] && [[ $5 != "" ]]; then
+    JAVA_HOME="$1"
+    JAVAC_HOME=$JAVA_HOME"c"
+    ITERATIONS=$2
+    ARRAY_SIZE=$3
+    WARMUP_ITERATIONS=$4
+    MEASURED_ITERATIONS=$5
+    echo "Iterations set: $ITERATIONS, Array size: $ARRAY_SIZE, Warmups: $WARMUP_ITERATIONS, Measures: $MEASURED_ITERATIONS"
+else
+    echo -e "Usage ./run_all.sh <java_path> <iterations> <array_size> <warmup_iterations> <measured_iterations>"
+    exit -1
+fi
+
+dir="$(pwd)"
+echo "dir: $dir"
+srcDir="$(dirname "$(pwd)")"
+srcDir=$srcDir"/src"
+echo "srcDir: $srcDir"
+echo "Compiling Java files..."
+
+$JAVAC_HOME $srcDir/*.java
+echo "Compiled Java files."
+
+echo "Current dir: $(pwd)"
+rm -r *.csv
+
+declare -a dataType=("Int" 
+				"Long"
+                "Float"
+				"Double")
+
+declare -a operationType=("Add"
+				"Sub"
+                "Mul"
+				"Div")
+
+# Call like runProgram "filename" ""
+runProgram ()
+{
+    # Set the filename depending on the parameter passed on; i.e. -Xjit:disableAutoSIMD
+    if [[ $2 == "" ]]; then
+        currFileName="$1.csv"
+    else
+        currFileName="$1_master.csv"
+        
+    fi
+
+    echo "Current filename: $currFileName"
+    echo "Warmup_time, Measured_time" |& tee -a $currFileName
+
+    x=1
+    while [ $x -le $ITERATIONS ]
+    do
+        # Execute java class with SIMD
+        if [[ $2 == "" ]]; then
+            ## do the main thing here
+            $JAVA_HOME -Xms4G -Xmx4G -cp $srcDir VectorizationMicroBenchmark $i $j $ARRAY_SIZE $WARMUP_ITERATIONS $MEASURED_ITERATIONS |& tee -a $currFileName
+        else
+            #-Xjit:disableAutoSIMD
+            $JAVA_HOME -Xjit:disableAutoSIMD -Xms4G -Xmx4G -cp $srcDir VectorizationMicroBenchmark $i $j $ARRAY_SIZE $WARMUP_ITERATIONS $MEASURED_ITERATIONS |& tee -a $currFileName
+        fi
+    
+        x=$(( $x + 1 ))
+    done
+}
+
+# Loop through the above array
+for i in "${dataType[@]}"
+do
+    for j in "${operationType[@]}"
+    do
+        if ([ "$i" == "Int" ] && [ "$j" == "Div" ]) || ([ "$i" == "Long" ] && [ "$j" == "Mul" ]) || ([ "$i" == "Long" ] && [ "$j" == "Div" ]); then
+            echo "Skipping $i $j"
+        else
+            echo "Running vector- $i $j"
+            runProgram "$i$j"
+            runProgram "$i$j" "-Xjit:disableAutoSIMD"
+                
+        fi
+    done
+done
--- src/VectorizationMicroBenchmark.java
+++ src/VectorizationMicroBenchmark.java
@@ -1,128 +1,162 @@
 public class VectorizationMicroBenchmark {
-    static int N = 1024;
-    static int[] a_i = new int[N];
-    static int[] b_i = new int[N];
-    static long[] a_l = new long[N];
-    static long[] b_l = new long[N];
-    static float[] a_f = new float[N];
-    static float[] b_f = new float[N];
-    static double[] a_d = new double[N];
-    static double[] b_d = new double[N];
+    static int N;
+    static int[] a_i;
+    static int[] b_i;
+    static long[] a_l;
+    static long[] b_l;
+    static float[] a_f;
+    static float[] b_f;
+    static double[] a_d;
+    static double[] b_d;
+
+    static long warmup_iteration;
+    static long measured_iteration;
+    
+    // Parse strings like 10^6 into 1000000
+    static long parseArgs(String s) {
+        if (s.contains("^")) {
+            double base = Double.parseDouble(s.substring(0, s.indexOf('^')));
+            double power = Double.parseDouble(s.substring(s.indexOf('^') + 1));
+            return (long) Math.pow(base, power);
+        }
+        return Long.parseLong(s);
+    }
 
     public static void main(String[] args) throws Exception {
 
-        if (args.length != 2) {
-            System.out.println("Usage: java <data-type> <operation>\n" +
+        if (args.length != 5) {
+            System.out.println("Usage: java <data-type> <operation> <array_size> " +
+                    "<warmup_iteration> <measured_iteration>\n" +
                     "Supported data-types: Int, Long, Float, Double.\n" +
                     "Supported operations: Add, Sub, Mul, Div.\n" +
-                    "Un-supported combinations: \"Int Div\", \"Long Div\", \" Long Mul\" ");
+                    "Un-supported combinations: \"Int Div\", \"Long Div\", \"Long Mul\" ");
             return;
         }
         else if ((args[0].equals("Long") && args[1].equals("Mul")) ||
                 (args[0].equals("Int") && args[1].equals("Div")) ||
                 (args[0].equals("Long") && args[1].equals("Div"))) {
             System.out.println("Operation '" + args[1] + "' of data-type '" + args[0] + "' is not supported.");
+            System.out.println("Usage: java <data-type> <operation> <array_size> " +
+                    "<warmup_iteration> <measured_iteration>\n" +
+                    "Supported data-types: Int, Long, Float, Double.\n" +
+                    "Supported operations: Add, Sub, Mul, Div.\n" +
+                    "Un-supported combinations: \"Int Div\", \"Long Div\", \"Long Mul\" ");
             return;
         }
 
-        boolean allOp = args[0].equals("All");
+        String dataType = args[0];
+        String opType = args[1];
+
+        N = Integer.parseInt(args[2]);
+		a_i = new int[N];
+		b_i = new int[N];
+		a_l = new long[N];
+		b_l = new long[N];
+		a_f = new float[N];
+		b_f = new float[N];
+		a_d = new double[N];
+		b_d = new double[N];
+
+        String warmupArg = args[3];
+        String measuredArg = args[4];
+        warmup_iteration = parseArgs(warmupArg);
+        measured_iteration = parseArgs(measuredArg);
+
+        boolean allOp = dataType.equals("All");
 
         // Fill-up the respective arrays
         for (int i = 0; i < N; i++) {
-            if (args[0].equals("Int") || allOp) {
+            if (dataType.equals("Int") || allOp) {
                 a_i[i] = i;
                 b_i[i] = N - 1 - i;
             }
-            else if (args[0].equals("Long") || allOp) {
+            else if (dataType.equals("Long") || allOp) {
                 a_l[i] = i;
                 b_l[i] = N - 1 - i;
             }
-            else if (args[0].equals("Float") || allOp) {
+            else if (dataType.equals("Float") || allOp) {
                 a_f[i] = (float) i;
                 b_f[i] = (float) (N - 1 - i);
             }
-            else if (args[0].equals("Double") || allOp) {
+            else if (dataType.equals("Double") || allOp) {
                 a_d[i] = (double) i;
                 b_d[i] = (double) (N - 1 - i);
             }
-
         }
 
         double warmup_time = 0;
         double measured_time = 0;
-        long warmup_iteration = 1000 * 1000;
-        long measured_iteration = 1000 * 1000 * 10;
 
-        // repeatedly invoke the method under test. this
-        // causes the JIT compiler to optimize the method
+        // Repeatedly invoke the method under test to
+        // initiate the JIT compiler to optimize the method
         for (long i = 0; i < (warmup_iteration + measured_iteration); i++) {
 
-            if (args[0].equals("Int") && args[1].equals("Add")) {
+            if (dataType.equals("Int") && opType.equals("Add")) {
                 if (i < warmup_iteration) warmup_time += IntOp.addI(a_i, b_i);
                 else measured_time += IntOp.addI(a_i, b_i);
             }
 
-            else if (args[0].equals("Int") && args[1].equals("Sub")) {
+            else if (dataType.equals("Int") && opType.equals("Sub")) {
                 if (i < warmup_iteration) warmup_time += IntOp.subI(a_i, b_i);
                 else measured_time += IntOp.subI(a_i, b_i);
             }
 
-            else if (args[0].equals("Int") && args[1].equals("Mul")) {
+            else if (dataType.equals("Int") && opType.equals("Mul")) {
                 if (i < warmup_iteration) warmup_time += IntOp.mulI(a_i, b_i);
                 else measured_time += IntOp.mulI(a_i, b_i);
             }
 
-            else if (args[0].equals("Long") && args[1].equals("Add")) {
+            else if (dataType.equals("Long") && opType.equals("Add")) {
                 if (i < warmup_iteration) warmup_time += LongOp.addL(a_l, b_l);
                 else measured_time += LongOp.addL(a_l, b_l);
             }
 
-            else if (args[0].equals("Long") && args[1].equals("Sub")) {
+            else if (dataType.equals("Long") && opType.equals("Sub")) {
                 if (i < warmup_iteration) warmup_time += LongOp.subL(a_l, b_l);
                 else measured_time += LongOp.subL(a_l, b_l);
             }
 
-            else if (args[0].equals("Float") && args[1].equals("Add")) {
+            else if (dataType.equals("Float") && opType.equals("Add")) {
                 if (i < warmup_iteration) warmup_time += FloatOp.addF(a_f, b_f);
                 else measured_time += FloatOp.addF(a_f, b_f);
             }
 
-            else if (args[0].equals("Float") && args[1].equals("Sub")) {
+            else if (dataType.equals("Float") && opType.equals("Sub")) {
                 if (i < warmup_iteration) warmup_time += FloatOp.subF(a_f, b_f);
                 else measured_time += FloatOp.subF(a_f, b_f);
             }
 
-            else if (args[0].equals("Float") && args[1].equals("Mul")) {
+            else if (dataType.equals("Float") && opType.equals("Mul")) {
                 if (i < warmup_iteration) warmup_time += FloatOp.mulF(a_f, b_f);
                 else measured_time += FloatOp.mulF(a_f, b_f);
             }
 
-            else if (args[0].equals("Float") && args[1].equals("Div")) {
+            else if (dataType.equals("Float") && opType.equals("Div")) {
                 if (i < warmup_iteration) warmup_time += FloatOp.divF(a_f, b_f);
                 else measured_time += FloatOp.divF(a_f, b_f);
             }
 
-            else if (args[0].equals("Double") && args[1].equals("Add")) {
+            else if (dataType.equals("Double") && opType.equals("Add")) {
                 if (i < warmup_iteration) warmup_time += DoubleOp.addD(a_d, b_d);
                 else measured_time += DoubleOp.addD(a_d, b_d);
             }
 
-            else if (args[0].equals("Double") && args[1].equals("Sub")) {
+            else if (dataType.equals("Double") && opType.equals("Sub")) {
                 if (i < warmup_iteration) warmup_time += DoubleOp.subD(a_d, b_d);
                 else measured_time += DoubleOp.subD(a_d, b_d);
             }
 
-            else if (args[0].equals("Double") && args[1].equals("Mul")) {
+            else if (dataType.equals("Double") && opType.equals("Mul")) {
                 if (i < warmup_iteration) warmup_time += DoubleOp.mulD(a_d, b_d);
                 else measured_time += DoubleOp.mulD(a_d, b_d);
             }
 
-            else if (args[0].equals("Double") && args[1].equals("Div")) {
+            else if (dataType.equals("Double") && opType.equals("Div")) {
                 if (i < warmup_iteration) warmup_time += DoubleOp.divD(a_d, b_d);
                 else measured_time += DoubleOp.divD(a_d, b_d);
             }
 
-            else if (args[0].equals("All") && args[1].equals("Op")) {
+            else if (dataType.equals("All") && opType.equals("Op")) {
                 if (i < warmup_iteration) {
                     warmup_time += IntOp.addI(a_i, b_i);
                     warmup_time += IntOp.subI(a_i, b_i);
